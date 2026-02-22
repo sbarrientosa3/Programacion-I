107df4d8eaf94fb70fbf1be3726c5a5fb00c3ea8
sbarrientosa3 <sbarrientosa3@miumg.edu.gt>
2026-02-21 22:09:06 -0600

    10-valor-booleano-texto.cpp

---
+++ Tarea-II-Tipos-de-Variables/Ejercicios/10-valor-booleano-texto.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    bool variable = false;
+    cout << "El valor usando boolalpha es: " << boolalpha << variable << endl;
+    return 0;
+}
