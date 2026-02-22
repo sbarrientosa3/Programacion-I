eb9650e5922b1dac8d51208b5a96572df5cc6ac6
sbarrientosa3 <sbarrientosa3@miumg.edu.gt>
2026-02-21 22:00:57 -0600

    1-edad.cpp

---
+++ Tarea-II-Tipos-de-Variables/Ejercicios/1-edad.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int edad;
+    cout << "Ingrese su edad actual: ";
+    cin >> edad;
+    cout << "Dentro de 5 años tendra: " << edad + 5 << " años." << endl;
+    return 0;
+}
