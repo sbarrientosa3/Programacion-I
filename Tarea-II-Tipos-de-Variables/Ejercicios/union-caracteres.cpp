c3515c940ea069c5f76cf41efe19187839595779
sbarrientosa3 <sbarrientosa3@miumg.edu.gt>
2026-02-21 21:56:20 -0600

    union-caracteres.cpp

---
+++ Tarea-II-Tipos-de-Variables/Ejercicios/union-caracteres.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char letra1, letra2;
+    cout << "Ingrese la primera letra: ";
+    cin >> letra1;
+    cout << "Ingrese la segunda letra: ";
+    cin >> letra2;
+    cout << "Resultado: " << letra1 << letra2 << endl;
+    return 0;
+}
