81901c2fead8c1917b91c03307227395eb5946a4
sbarrientosa3 <sbarrientosa3@miumg.edu.gt>
2026-02-21 22:12:55 -0600

    Create 11-bienvenida.cpp

---
+++ Tarea-II-Tipos-de-Variables/Ejercicios/11-bienvenida.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string> 
+using namespace std;
+
+int main() {
+    string nombre;
+    cout << "Ingrese su nombre: ";
+    cin >> nombre;
+    cout << "Bienvenido(a) al curso de Programacion 1 de la carrera de ingenieria de UMG, " << nombre << "!" << endl;
+    return 0;
+}
