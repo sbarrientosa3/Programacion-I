4e9d54068f9c9eced1ffe39ad458752f49ff995c
sbarrientosa3 <sbarrientosa3@miumg.edu.gt>
2026-02-21 21:44:05 -0600

    Int.cpp

--- Tarea-II-Tipos-de-Variables/Ejercicios/Int.cpp
+++ Tarea-II-Tipos-de-Variables/Ejercicios/Int.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int num1, num2;
+    cout << "Ingrese el primer numero: ";
+    cin >> num1;
+    cout << "Ingrese el segundo numero: ";
+    cin >> num2;
+    cout << "Suma: " << num1 + num2 << endl;
+    cout << "Resta: " << num1 - num2 << endl;
+    cout << "Multiplicacion: " << num1 * num2 << endl;
+    return 0;
+}
