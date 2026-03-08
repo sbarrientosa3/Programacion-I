8fd3f27998581c5e92a450f3ccdd6a97a33b10eb
sbarrientosa3 <sbarrientosa3@miumg.edu.gt>
2026-03-08 10:13:06 -0600

    Creacion de archivo para el ejercicio en clase

---
+++ Tarea IV/Ejercicio.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int edad = 18;
+
+    if (edad >= 18) {
+        cout <<"Es mayor de edad";
+    return 0;
+    }
+    cout <<"Es menor de edad";
+return 0;
+}
