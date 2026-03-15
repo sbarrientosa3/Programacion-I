97892236d124a33b9c71a6d1861b7fd3faa3b576
sbarrientosa3 <sbarrientosa3@miumg.edu.gt>
2026-03-14 22:46:11 -0600

    Insert del codigo en el archivo

---
+++ Tarea_Puerta_Logica/puerta_logica.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string nombre;
+    int edad;
+    double promedio;
+    int miembroclub;
+    bool acceso = false;
+
+    cout << "Ingrese nombre completo: ";
+    getline(cin, nombre);
+    cout << "Ingrese su edad: ";
+    cin >> edad;
+    cout << "Ingrese su promedio academico: ";
+    cin >> promedio;
+    cout << "Si es parte del club de programacion utilice el 1, de lo contrario utilice el 0: ";
+    cin >> miembroclub;
+
+    if (nombre == "David Alvarez")
+    {
+        cout << "Acceso especial concedido";
+        return 0;
+    }
+
+
+    if (edad >= 18 && promedio >= 85)
+    {
+        acceso = true;
+    }
+    else if (miembroclub == 1 && promedio >= 75)
+    {
+        acceso = true;
+    }
+    else if (promedio >= 95)
+    {
+        acceso = true;
+    }
+
+
+    if (acceso)
+    {
+        size_t posicionEspacio = nombre.find(' ');
+        string primerNombre = nombre.substr(0, posicionEspacio);
+        cout << "Bienvenido " << primerNombre << endl;
+        cout << "Acceso autorizado" << endl;
+        return 0;
+    }
+    else
+    {
+        cout << "Acceso denegado" << endl;
+        return 0;
+    }
+
+    return 0;
+}
