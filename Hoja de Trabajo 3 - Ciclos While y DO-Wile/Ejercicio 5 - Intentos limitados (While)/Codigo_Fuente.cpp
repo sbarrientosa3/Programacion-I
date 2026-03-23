489cd85a34945d0d3be23e334388d86df9cbc9d5
sbarrientosa3 <sbarrientosa3@miumg.edu.gt>
2026-03-22 20:33:32 -0600

    Creacion de codigos fuente

--- Hoja de Trabajo 3 - Ciclos While y DO-Wile/Ejercicio 5 - Intentos limitados (While)/Codigo_Fuente.cpp
+++ Hoja de Trabajo 3 - Ciclos While y DO-Wile/Ejercicio 5 - Intentos limitados (While)/Codigo_Fuente.cpp
@@ -1,3 +1,28 @@
-//
-// Created by ferna on 22/03/2026.
-//
+#include <iostream>
+using namespace std;
+
+int main() {
+    string password;
+    int intentos = 1;
+    bool acierto = false;
+
+    while (intentos <= 3 && !acierto) {
+        cout << "Intento " << intentos << "Ingrese contrasena: ";
+        cin >> password;
+
+        if (password == "1234") {
+            acierto = true;
+        } else {
+            cout << "Incorrecto." << endl;
+            intentos++;
+        }
+    }
+
+    if (acierto) {
+        cout << "Bienvenido" << endl;
+    } else {
+        cout << "Acceso denegado" << endl;
+    }
+
+    return 0;
+}
--- Hoja de Trabajo 3 - Ciclos While y DO-Wile/Ejercicio 6 - Piramide de asteriscos (While o Do-While)/Codigo_Fuente.cpp
+++ Hoja de Trabajo 3 - Ciclos While y DO-Wile/Ejercicio 6 - Piramide de asteriscos (While o Do-While)/Codigo_Fuente.cpp
@@ -1,3 +1,26 @@
-//
-// Created by ferna on 22/03/2026.
-//
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, fila = 1;
+
+    cout << "Ingrese la altura de la piramide: ";
+    cin >> n;
+
+    while (fila <= n) {
+        int espacios = n - fila;
+        while (espacios > 0) {
+            cout << " ";
+            espacios--;
+        }
+        int asteriscos = 1;
+        while (asteriscos <= fila) {
+            cout << "*";
+            asteriscos++;
+        }
+
+        cout << endl;
+        fila++;
+    }
+    return 0;
+}
